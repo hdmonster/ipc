f00f1dc6a9f729fa533bfc94856b7e8483c4fcdb
Haydar Dzaky <heydardzakis@gmail.com>
2023-11-30 14:04:27 +0800

    add redux config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import { Provider } from 'react-redux'
+
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename='/'>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
