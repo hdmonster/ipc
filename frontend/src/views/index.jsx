4379c27d1602b3e6e3294355d2fdb0ede75eac0c
Haydar Dzaky <heydardzakis@gmail.com>
2023-11-30 14:04:02 +0800

    update homepage

--- frontend/src/views/index.jsx
+++ frontend/src/views/index.jsx
@@ -1,17 +1,50 @@
-import { Button, FormControl, Grid, Typography } from '@mui/material'
+import axios from '../lib/axios'
+import { Button, Grid, TextField } from '@mui/material'
 import DragDropFile from '../components/DragDropFile/DragDropFile'
 import { useState } from 'react'
 
 export default function Index() {
   const [doc, setDoc] = useState(null)
+  const [pageNumber, setPageNumber] = useState({
+    start: null,
+    end: null,
+  })
 
   const handleFile = files => {
     setDoc(files[0])
     console.log(files)
   }
 
+  const handleChange = e => {
+    try {
+      const targetName = [e.target.name]
+      setPageNumber(prev => ({ ...prev, [targetName]: e.target.value }))
+    } catch (err) {
+      console.log('Error', err)
+    }
+  }
+
   const handleSubmit = async e => {
     e.preventDefault()
+
+    const formData = new FormData()
+    formData.append('file', doc)
+    formData.append('start', pageNumber.start)
+    formData.append('end', pageNumber.end)
+
+    try {
+      const res = await axios.post('/check', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+
+      let resJson = JSON.parse(res.data)
+
+      alert(
+        `Your plagiarism score is ${resJson.score}!\nYour document is similar to ${resJson.similar_docs[0].title}`
+      )
+    } catch (e) {
+      alert('Error:', e)
+    }
   }
 
   return (
@@ -27,6 +60,32 @@ export default function Index() {
         >
           <Grid item xs={12}>
             <DragDropFile handleFile={handleFile} doc={doc} />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              id='start'
+              name='start'
+              label='Start page'
+              type='number'
+              placeholder='eg. 0'
+              onChange={handleChange}
+              variant='standard'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              id='end'
+              name='end'
+              label='End page'
+              type='number'
+              placeholder='eg. 10'
+              onChange={handleChange}
+              variant='standard'
+              fullWidth
+            />
           </Grid>
           <Grid item xs={6}>
             <Button type='submit' variant='contained' fullWidth>
